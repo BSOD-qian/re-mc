0461ef14d8a15f2da8b2499d6db509bc76d280e1
BSOD-qian <3348640434@qq.com>
2022-12-19 20:27:47 +0800

    test1

---
+++ versioncheck.js
@@ -0,0 +1,23 @@
+function vercheck(){
+    var xhr, data, version;
+    xhr = new XMLHttpRequest();
+    xhr.open("get", "./version.json");
+    xhr.onreadystatechange = function(){
+        if (xhr.readyState === 4) {
+            data = JSON.parse(xhr.responseText);
+            version = data.version;
+            if (version !== "0.16.2"){
+                mdui.snackbar({
+                    message: '检测到更新，版本' + data.version,
+                    buttonText: '刷新',
+                    onButtonClick: function(){
+                      location.reload();
+                    }
+                  });
+            };
+        }
+    }
+    xhr.send();
+};
+window.onload = vercheck();
+setInterval("vercheck();", 60000);
