466c6a2245fc4ec7ff3a1bf420110c5ed160de48
bbBSODdd <3348640434@qq.com>
2024-05-23 20:34:00 +0800

    20240523

--- versioncheck.js
+++ versioncheck.js
@@ -1,4 +1,4 @@
-var current = "2.0.4.1"
+var current = "2.0.5"
 document.getElementById('dialog_version').innerHTML = current;
 function vercheck() {
     var url = "./version.json?" + Math.random();
