76973f63af6a89b1d51f2a2ce138973be1ad835e
bbBSODdd <3348640434@qq.com>
2024-10-19 09:45:58 +0800

    2.0.7.1

--- versioncheck.js
+++ versioncheck.js
@@ -1,4 +1,4 @@
-var current = "2.0.7"
+var current = "2.0.7.1"
 document.getElementById('dialog_version').innerHTML = current;
 function vercheck() {
     var url = "./version.json?" + Math.random();
