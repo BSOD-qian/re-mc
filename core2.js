7bab43b505aaaa342081fa00e140ecf15e70c734
bbBSODdd <3348640434@qq.com>
2025-05-17 22:52:06 +0800

    202505172252

--- core2.js
+++ core2.js
@@ -1,4 +1,7 @@
 var userinput = 0;//用户输入变量
+var length = 0;//车体号长度
+var lineno = 0;//车体号线路号
+var carnoerror = 0;//车体号报错变量
 //url读取与分割
 var url = window.location.href;
 var canshu = url.split("?");
@@ -16,4 +19,23 @@ function wuc(){
 function receive(){
     userinput = document.getElementById('input').value;//为变量赋值
     document.title = userinput + " - Re:地铁计算器（Beta）"//更改网页标题
+    length = userinput.length;//获取车体号长度
+    lineno = userinput.substr(0, 2);
+    if (length != 0){
+        if (length == 5){
+            myear();//年份车体号
+        }else{
+            if (length == 6){
+                //线路号车体号
+            }else{
+                if (length == 7){
+                    pjl_or_sr();
+                }else{
+                    carnoerror = 1;//错误状态设为1
+                }
+            }
+        }
+    }else{
+        carnoerror = 1;//错误状态设为1
+    }
 }
